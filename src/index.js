d2a1467f79202957c8669de7f35128aa4bae6826
SOUnit <workbookwork7@gmail.com>
2021-03-01 20:40:05 +0900

    change App function to class based component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import { React, Component } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => {
-      console.log(position);
-    },
-    (err) => {
-      console.log(err);
-    }
-  );
+class App extends Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
 
-  return (
-    <div>
-      <p>test</p>
-      <SeasonDisplay />
-    </div>
-  );
-};
+    return (
+      <div>
+        <p>test</p>
+        <SeasonDisplay />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector('#root'));
