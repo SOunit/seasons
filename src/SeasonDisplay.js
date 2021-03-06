3e2f641f024cd2eaaa744ba8ec62a3df082f9e1f
SOUnit <workbookwork7@gmail.com>
2021-03-06 19:04:24 +0900

    added css

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './SeasonDisplay.css';
 
 const seasonConfig = {
   summer: {
@@ -24,10 +25,10 @@ const SeasonDisplay = (props) => {
   const { text, iconName } = seasonConfig[season];
 
   return (
-    <div>
-      <i className={`${iconName} icon`} />
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} massive icon left-icon`} />
       <h1>{text}</h1>
-      <i className={`${iconName} icon`} />
+      <i className={`${iconName} massive icon right-icon`} />
     </div>
   );
 };
